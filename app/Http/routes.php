40c63e67e979f6f5e9fc42333075707c943c85ed
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-09-07 20:19:26 +0700

    level 3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,29 +11,32 @@
 |
 */
 
-Route::get('/', function () {
+//Route::get('/', function () {
     //return view('welcome');
-	return "Hi Guys...";
-});
-Route::get('/contoh', function () {
-    return view('welcome');
-});
-Route::get('/admin/post', function () {
-    return "admin is here.";
-});
-Route::get('/about', function () {
-    return "Hi about page.";
-});
-Route::get('/contact', function () {
-    return "Hi, I am contact.";
-});
-Route::get('/post/{id}', function ($id) {
-    return "This is post number. " .$id;
-});
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This is post number. " .$id ." ".$name;
-});
-Route::get('admin/post/example', array('as'=>'admin.home', function() {
-	$url = route('admin.home');
-    return "this url is:". $url;
-}));
+//	return "Hi Guys...";
+//});
+//Route::get('/contoh', function () {
+ //   return view('welcome');
+//});
+//Route::get('/admin/post', function () {
+ //   return "admin is here.";
+//});
+//Route::get('/about', function () {
+ //   return "Hi about page.";
+//});
+//Route::get('/contact', function () {
+ //   return "Hi, I am contact.";
+//});
+//Route::get('/post/{id}', function ($id) {
+ //   return "This is post number. " .$id;
+//});
+//Route::get('/post/{id}/{name}', function ($id, $name) {
+ //   return "This is post number. " .$id ." ".$name;
+//});
+//Route::get('admin/post/example', array('as'=>'admin.home', function(){
+//	$url = route('admin.home');
+ //   return "this url iis:". $url;
+//}));
+//Route::get('/post/{id}', 'PostsController@index');
+
+Route::resource('posts','PostsController');
