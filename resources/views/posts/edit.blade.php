767983af348fa44b2cdcb93e5d74c3e8db01089e
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-09-28 19:54:40 +0700

    level 16.7

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,8 +6,9 @@
 
 	<h1>Edit Post</h1>
 	
-	<form action="/posts" method="post">
-		<input type="text" name="title" />
+	<form action="/posts/{{$post->id}}" method="post">
+		<input type="hidden" name="_method" value="PUT" />
+		<input type="text" name="title" value="{{$post->title}}" />
 		{{csrf_field()}}
 		<input type="submit" name="submit" />
 	</form>
