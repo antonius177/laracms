d4e7cad69a02b85e7130ad1f85b23d92d994f32a
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-09-28 18:44:03 +0700

    level 16.1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ use App\Video;
  //   return "Hi about page.";
 //});
 
-Route::get('/contact','PostsController@contact');
+//Route::get('/contact','PostsController@contact');
 
 //Route::get('/post/{id}', function ($id) {
  //   return "This is post number. " .$id;
@@ -50,82 +50,81 @@ Route::get('/contact','PostsController@contact');
 //Route::get('/contact', 'PostsController@contact');
 //Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
 
-Route::get('/', function(){
-	return view('Welcome');
-	});
+//Route::get('/', function(){
+//	return view('Welcome');
+//	});
 
-Route::get('/read', function(){
-	$posts = Post::all();
-	foreach($posts as $Post){
-		return $Post->title;
-	}
-});
+//Route::get('/read', function(){
+//	$posts = Post::all();
+	//foreach($posts as $Post){
+	//	return $Post->title;
+	//}
+//});
 
-Route::get('/find', function(){
-	$Post = Post::find(17);
-	return $Post->title;
+//Route::get('/find', function(){
+	//$Post = Post::find(17);
+	//return $Post->title;
 	
-});
-Route::get('/findwhere', function(){
-	$Posts = Post::where('is_admin', 1)->orderBy('id', 'desc')->take(17)->get();
-	return $Posts;
+//});
+//Route::get('/findwhere', function(){
+//	$Posts = Post::where('is_admin', 1)->orderBy('id', 'desc')->take(17)->get();
+//	return $Posts;
 	
-});
+//});
 
-Route::get('/basicinsert', function(){
-	$Post = new Post;
-	$Post->title = 'New Eloquent Title';
-	$Post->content = 'WOw Eloquent is real cool';
-	$Post->save();
+//Route::get('/basicinsert', function(){
+//	$Post = new Post;
+	//$Post->title = 'New Eloquent Title';
+//	$Post->content = 'WOw Eloquent is real cool';
+	//$Post->save();
+	//});
+
+//Route::get('/create', function(){
+	//Post::create(['title' => 'create method', 'content' => 'saya belajar banyak hari ini']);
+//});
+
+//Route::get('/basicupdate', function(){
+	//$Post = Post::find(17);
 	
-});
-
-Route::get('/create', function(){
-	Post::create(['title' => 'create method', 'content' => 'saya belajar banyak hari ini']);
-});
-
-Route::get('/basicupdate', function(){
-	$Post = Post::find(17);
+//	$Post->title = 'updated Eloquent title';
+	//$Post->content = 'Updated Eloquent content';
 	
-	$Post->title = 'updated Eloquent title';
-	$Post->content = 'Updated Eloquent content';
-	
-	$Post->save();
-});
+	//$Post->save();
+//});
 
-Route::get('/update', function(){
-	Post::where('id', 18)->where('is_admin',0)->update(['title' => 'new php title', 'content' => ' i love learning laravel']);
-});
-Route::get('/delete', function(){
-	$Post = Post::find(17);
-	$Post->delete();
-});
-Route::get('/delete2', function(){
-	Post::destroy(18,17);
-});
-Route::get('/delete3', function(){
-	Post::where('is_admin', 0)->delete();
-});
-Route::get('/softdelete', function(){
-	Post::find(20)->delete();
-});
-Route::get('/readsoftdelete', function(){
+//Route::get('/update', function(){
+	//Post::where('id', 18)->where('is_admin',0)->update(['title' => 'new php title', //'content' => ' i love learning laravel']);
+//});
+//Route::get('/delete', function(){
+	//$Post = Post::find(17);
+	//$Post->delete();
+//});
+//Route::get('/delete2', function(){
+	//Post::destroy(18,17);
+//});
+//Route::get('/delete3', function(){
+	//Post::where('is_admin', 0)->delete();
+//});
+//Route::get('/softdelete', function(){
+	//Post::find(20)->delete();
+//});
+//Route::get('/readsoftdelete', function(){
 	//$Post = Post::find(20);
 //	return $Post;
 	//$Post = Post::withTrashed()->where('id',20)->get();
 	//return $Post;
 	//$Post = Post::withTrashed()->get();
 	//return $Post;
-	$Post = Post::onlyTrashed()->get();
-	return $Post;
-});
+//	$Post = Post::onlyTrashed()->get();
+	//return $Post;
+//});
 
-Route::get('/restore', function(){
-	Post::withTrashed()->where('id',20)->restore();
-});
-Route::get('/forcedelete', function(){
-	Post::onlyTrashed()->where('is_admin',0)->forcedelete();
-});
+//Route::get('/restore', function(){
+	//Post::withTrashed()->where('id',20)->restore();
+//});
+//Route::get('/forcedelete', function(){
+	//Post::onlyTrashed()->where('is_admin',0)->forcedelete();
+//});
 
 /*
 -----------------------------------------------------
@@ -134,20 +133,20 @@ Eloquent Relationships
 */
 //one to one Relationships
 
-Route::get('User/{id}/Post', function($id){
-	return User::find($id)->Post->title;
-});
-Route::get('Post/{id}/User', function($id){
-	return Post::find($id)->User->name;
-});
+//Route::get('User/{id}/Post', function($id){
+//	return User::find($id)->Post->title;
+//});
+//Route::get('Post/{id}/User', function($id){
+//	return Post::find($id)->User->name;
+//});
 
-Route::get('/Posts', function(){
-	$user = user :: find(1);
+//Route::get('/Posts', function(){
+	//$user = user :: find(1);
 	
-	foreach($user->posts as $post){
-		echo $post->title;
-	}
-});
+	//foreach($user->posts as $post){
+		//echo $post->title;
+	//}
+//});
 
 //Route::get('user/Pivot', function(){
 	//$user = User::find(1);
@@ -157,63 +156,63 @@ Route::get('/Posts', function(){
 	//}
 //});
 
-Route::get('user/Pivot', function(){
-	$user = User::find(1);
+//Route::get('user/Pivot', function(){
+	//$user = User::find(1);
 	
-	foreach($user->roles as $role){
-		echo $role->Pivot->created_at;
+	//foreach($user->roles as $role){
 		//echo $role->Pivot->created_at;
-	}
-});
-Route::get('/user/country', function (){
-	$country = Country::find(6);
+		//echo $role->Pivot->created_at;
+	//}
+//});
+//Route::get('/user/country', function (){
+	//$country = Country::find(6);
 	
-	foreach($country->posts as $Post){
-		return $Post->title;
+	//foreach($country->posts as $Post){
+		//return $Post->title;
 		
-	}
-});
+	//}
+//});
 
-Route::get('/post/photos', function(){
-	$post = Post::find(1);
+//Route::get('/post/photos', function(){
+	//$post = Post::find(1);
 	
-	foreach($post->photos as $photo){
-		return $photo->path;
-	}
-});
-Route::get('/user/photos', function(){
-	$user = User::find(1);
-	foreach($user->photos as $photo){
-		return $photo->path;
-	}
-});
+	//foreach($post->photos as $photo){
+		//return $photo->path;
+	//}
+//});
+//Route::get('/user/photos', function(){
+	//$user = User::find(1);
+	//foreach($user->photos as $photo){
+		//return $photo->path;
+	//}
+//});
 
-Route::get('photo/{id}/post', function($id){
-		$Photo = Photo::findOrFail($id);
-		return $Photo->imageable;
-	});
+//Route::get('photo/{id}/post', function($id){
+	//	$Photo = Photo::findOrFail($id);
+		//return $Photo->imageable;
+	//});
 
-Route::get('/post/tag', function (){ 
-	$Post = Post::find(1);
-	foreach($Post->tags as $Tag){
-		echo $Tag->name;
-	}
-});
+//Route::get('/post/tag', function (){ 
+	//$Post = Post::find(1);
+	//foreach($Post->tags as $Tag){
+		//echo $Tag->name;
+	//}
+//});
 
-Route::get('/tag/post', function(){
-	$Tag = Tag:: find(1);
-	foreach($Tag->Posts as $Post){
-		echo $Post->title;
-	}
-});
+//Route::get('/tag/post', function(){
+	//$Tag = Tag:: find(1);
+	//foreach($Tag->Posts as $Post){
+		//echo $Post->title;
+	//}
+//});
 	
-Route::get('/user/{id}/role', function ($id){
-	$user = User::find($id)->roles()->orderBy('id', 'desc')->get();
-	return $user;
+//Route::get('/user/{id}/role', function ($id){
+	//$user = User::find($id)->roles()->orderBy('id', 'desc')->get();
+	//return $user;
 	//foreach($user->roles as $role){
 		//return $role->name;
 	//}
-});
+//});
 	
 	
 /*
@@ -242,5 +241,11 @@ DATABASE raw SQL Queries
 //		$delete = DB::delete("DELETE FROM posts WHERE id =?",[1]);
 //		return $delete;
 //	});	
-	
+
+/*
+|---------------------------------------------------------------------------------------------| CRUD Appications
+|---------------------------------------------------------------------------------------------
+*/
+
+Route::resource('/post', 'PostsController');
 	
