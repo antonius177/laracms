79ccd683f342ba7a4596abeb4a7834081a5b199e
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-10-05 16:41:04 +0700

    level 17.1

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,14 +4,18 @@
 
 @section('content')
 	<!-- <form action="/posts" method="post"> -->
-	{!! Form::open() !!}
-
-	<form action="/posts" method="post">
-		<input type="text" name="title" />
-		{{csrf_field()}}
-		<input type="submit" name="submit" />
-	</form>
+	{!! Form::open(['method'=>'POST','action'=>'PostsController@store']) !!}
 	
+	<div class="form-group">
+		{!! Form::label('title', 'Title') !!}
+		{!! Form::text('title', null, ['class'=>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+		{!! Form::submit('Create Post', ['class'=>'btn btn-primary'])!!}
+	</div>
+	
+	{!! Form::close() !!}
 		
 @stop
 
