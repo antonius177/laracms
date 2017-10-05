769dbba203a7f656b11d03c7efbb54ebaa83d978
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-10-05 18:26:18 +0700

    level 17.2

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,12 +5,31 @@
 @section('content')
 
 	<h1>Edit Post</h1>
-	
+	<!-- <form action="/posts" method="post"> 
 	<form action="/posts/{{$post->id}}" method="post">
 		<input type="hidden" name="_method" value="PUT" />
 		<input type="text" name="title" value="{{$post->title}}" />
 		{{csrf_field()}}
 		<input type="submit" name="submit" />
+	</form>
+	-->
+	{!! Form::model($post, ['method'=>'PATCH', 'action'=>['PostsController@update', $post->id]]) !!}
+	
+	{!! Form::label('title', 'Title') !!}
+	{!! Form::text('title', null, ['class'=>'form-control']) !!}
+	
+	{!! Form::submit('Update Post', ['class'=>'btn btn-info'])!!}
+	
+	{!! Form::close()!!}
+		
+		{!! Form::open(['method'=>'DELETE', 'action'=>['PostsController@destroy', $post->id]]) !!}
+		{!! Form::submit('Delete Post', ['class'=>'btn btn-danger'])!!}
+		{!! Form::close()!!}
+		
+		<form action="/posts/{{$post->id}}" method="post">
+		<input type="hidden" name="_method" value="DELETE" />
+		{{csrf_field()}}
+		<input type="submit" name="DELETE" />
 	</form>
 	
 @stop
